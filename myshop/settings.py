08cc47c63efc19a51778ed812b02c53d5987e8e1
micahla591 <a0548562302@gmail.com>
2024-10-08 20:32:54 +0300

    setting

--- myshop/settings.py
+++ myshop/settings.py
@@ -26,11 +26,11 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 
 CSRF_COOKIE_SECURE = True
 
-ALLOWED_HOSTS = ["my-shop-2k5m.onrender.com"]
-CSRF_TRUSTED_ORIGINS = ["https://my-shop-2k5m.onrender.com/"]
-CORS_ALLOWED_ORIGINS = [
-    "https://my-shop-2k5m.onrender.com/",
-]
+# ALLOWED_HOSTS = ["my-shop-2k5m.onrender.com"]
+# CSRF_TRUSTED_ORIGINS = ["https://my-shop-2k5m.onrender.com/"]
+# CORS_ALLOWED_ORIGINS = [
+#    "https://my-shop-2k5m.onrender.com/",
+# ]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
