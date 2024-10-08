030b6dd6c6381df29e121066ad34a0dbbcbd0b02
micahla591 <a0548562302@gmail.com>
2024-10-08 20:56:25 +0300

    setting load

--- myshop/settings.py
+++ myshop/settings.py
@@ -13,7 +13,10 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 import os
 from pathlib import Path
 from decouple import config
+from dotenv import load_dotenv
 
+# Load environment variables from .env file
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
