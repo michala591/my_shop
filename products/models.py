81ee23ffd906fdb099a4157da3b611bf9666f001
micahla591 <a0548562302@gmail.com>
2024-10-28 21:15:44 +0200

    added cart+ admin

--- products/models.py
+++ products/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from cart.models import Cart
+
 
 class Category(models.Model):
     name = models.CharField(max_length=255)
@@ -15,6 +17,7 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     category = models.ManyToManyField(Category, related_name="products")
     image = models.ImageField(null=True, blank=True, default="/placeholder.png")
+    cart = models.ManyToManyField(Cart, related_name="products")
 
     def __str__(self):
         return f"{self.name} - {self.price}"
